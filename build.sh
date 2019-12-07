c9bcad02afc1f76d5530ea9f143420511e34521f
Paul Moore <p.f.moore@gmail.com>
2019-12-07 15:54:04 +0000

    Add wildcard support

--- build.sh
+++ build.sh
@@ -7,9 +7,14 @@ wget http://ftp.gnu.org/gnu/sed/sed-${SED_VER}.tar.xz
 tar xf sed-${SED_VER}.tar.xz
 cd sed-${SED_VER}
 
-mingw64-configure --disable-nls --disable-i18n
+# Wildcard expansion
+echo "int _dowildcard = -1;" >wild.c
+x86_64-w64-mingw32-gcc -c wild.c
+
+mingw64-configure LIBS="$(pwd)/wild.o" --disable-nls --disable-i18n
 
 make sed/sed.exe
+x86_64-w64-mingw32-strip -s sed/sed.exe
 
 TARGET=../sed-${SED_VER}.zip
 zip -j9 ${TARGET} sed/sed.exe
