7fa30e1cd74b96ab2c2fae900c8e7686cd10ccc8
Paul Moore <p.f.moore@gmail.com>
2019-12-11 19:48:27 +0000

    Use supplied wildcard config library

--- build.sh
+++ build.sh
@@ -7,11 +7,7 @@ wget http://ftp.gnu.org/gnu/sed/sed-${SED_VER}.tar.xz
 tar xf sed-${SED_VER}.tar.xz
 cd sed-${SED_VER}
 
-# Wildcard expansion
-echo "int _dowildcard = -1;" >wild.c
-x86_64-w64-mingw32-gcc -c wild.c
-
-mingw64-configure LIBS="$(pwd)/wild.o" --disable-nls --disable-i18n
+mingw64-configure LIBS="/usr/x86_64-w64-mingw32/sys-root/mingw/lib/CRT_glob.o" --disable-nls --disable-i18n
 
 make sed/sed.exe
 x86_64-w64-mingw32-strip -s sed/sed.exe
